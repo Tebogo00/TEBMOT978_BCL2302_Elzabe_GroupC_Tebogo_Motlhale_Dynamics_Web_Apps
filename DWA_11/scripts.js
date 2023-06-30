724b2b91d6a3eda3fcadf86346c895d7aae0bd73
Tebogo00 <motlhalet420@gmail.com>
2023-06-30 11:14:49 +0200

    DWA_11

--- DWA_11/scripts.js
+++ DWA_11/scripts.js
@@ -1,24 +1,34 @@
-// Define constants
+// The max and min number allowed for the range.
 const MAX_NUMBER = 15;
 const MIN_NUMBER = -5;
-const STEP_AMOUNT = 1;
-// Define action types
+const STEP_AMOUNT = 1;// The amount by which the number should increase or decrease.
+
+
+// ActionTypes is an object that defines action types for a counter feature.
+
 const ActionTypes = {
-  INCREMENT: "INCREMENT",
-  DECREMENT: "DECREMENT",
-  RESET: "RESET",
+  INCREMENT: "INCREMENT",// Represents the action type for incrementing the counter.
+  DECREMENT: "DECREMENT",// Represents the action type for decrementing the counter.
+  RESET: "RESET", // Represents the action type for resetting the counter.
 };
-// Define action creators
+
+
+// This function creates an action object to increment, decrement and reset the counter.
 function incrementCounter() {
   return { type: ActionTypes.INCREMENT };
 }
+
 function decrementCounter() {
   return { type: ActionTypes.DECREMENT };
 }
+
 function resetCounter() {
   return { type: ActionTypes.RESET };
 }
-// Define reducer function
+
+
+// This function is a reducer that handles actions related to the counter.
+
 function counterReducer(state = 0, action) {
   switch (action.type) {
     case ActionTypes.INCREMENT:
@@ -31,13 +41,18 @@ function counterReducer(state = 0, action) {
       return state;
   }
 }
-// Create a Redux store
+
+
+// Create a Redux store using the counterReducer
 const store = Redux.createStore(counterReducer);
-// Update the UI with the initial state
-let currentValue = store.getState();
+let currentValue = store.getState();// Variable to hold the current value of the state
+
+// Function to update the user interface based on the current state
 function updateUI() {
-  currentValue = store.getState();
-  number.value = currentValue.toString();
+currentValue = store.getState();  // Update the current value with the state from the Redux store
+number.value = currentValue.toString();// Update the value displayed in the UI input element
+
+  // Disable or enable the subtract and add buttons based on the current value
   if (currentValue <= MIN_NUMBER) {
     subtract.disabled = true;
   } else if (currentValue >= MAX_NUMBER) {
@@ -46,24 +61,32 @@ function updateUI() {
     subtract.disabled = false;
     add.disabled = false;
   }
-  console.log(currentValue);
+    console.log(currentValue);
 }
-// Subscribe to state changes and update the UI
+
+
+// Subscribe to state changes in the Redux store and update the UI
 store.subscribe(updateUI);
-// Attach event listeners to the buttons
+
+
+// Get the DOM elements for the number input, subtract button, and add button
 const number = document.querySelector('[data-key="number"]');
 const subtract = document.querySelector('[data-key="subtract"]');
 const add = document.querySelector('[data-key="add"]');
+
+// Add an event listener to the subtract and add button
 subtract.addEventListener("click", () => {
-  store.dispatch(decrementCounter());
+  store.dispatch(decrementCounter());// Dispatch an action to decrement the counter
 });
 add.addEventListener("click", () => {
-  store.dispatch(incrementCounter());
+  store.dispatch(incrementCounter());// Dispatch an action to increment the counter
 });
-const resetButton = document.querySelector("sl-button");
+
+const resetButton = document.querySelector("sl-button");// Get the DOM element for the reset button
 resetButton.addEventListener("click", () => {
-  store.dispatch(resetCounter());
-  alert("The counter has been reset.");
+store.dispatch(resetCounter());
+alert("The counter has been reset.");
 });
-// Initialize the UI
+
+// Update the UI with the initial state
 updateUI();
